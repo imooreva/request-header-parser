c2fe73317cff9cea1b7fef25e66566a4a0cbbd2a
Ian Moore <ianmooregis@gmail.com>
2017-02-12 14:31:11 -0600

    trying alternative for fetching IP

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
+//const requestIp = require('request-ip');
 const PORT = process.env.PORT || 3000; //used for heroku
 
 var app = express();
 app.use(express.static('./public'));
+//app.use(requestIp.mw());
+//app.use((req, res)=> {
+//    var ip = req.clientIp;
+//    res.end(ip);
+//});
+
 app.listen(PORT, () => console.log('Express server is up on port', PORT));
 
 app.get('/headers', (req, res) => {
     //console.log(req.headers);
-    let ipaddress = req.headers['x-real-ip'] || req.connection.remoteAddress;
+    let ipaddress = req.headers['X-Client-IP'] || req.connection.remoteAddress;
     res.send({
         ipaddress,
         language: req.headers['accept-language'],
